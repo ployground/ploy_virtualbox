de15ca41ef1994d126b593e8c6bb21765b3906e6
Florian Schulze <florian.schulze@gmx.net>
2014-06-16 18:12:45 +0200

    We need the next release.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     namespace_packages=['mr'],
     install_requires=[
         'setuptools',
-        'mr.awsome >= 1.0rc6',
+        'mr.awsome >= 1.0rc8',
         'lazy'],
     entry_points="""
         [mr.awsome.plugins]
