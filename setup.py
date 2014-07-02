134add43500762ee6fe0a93b67cdcc42a51fd481
Florian Schulze <florian.schulze@gmx.net>
2014-07-02 15:03:53 +0200

    Add license.

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ setup(
     name="ploy_virtualbox",
     author='Florian Schulze',
     author_email='florian.schulze@gmx.net',
+    license="BSD 3-Clause License",
     url='http://github.com/ployground/ploy_virtualbox',
     include_package_data=True,
     zip_safe=False,
