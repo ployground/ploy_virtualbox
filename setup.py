84f012113d9130f8d784c06338b7b1aef0b3ba1c
Florian Schulze <mail@florian-schulze.net>
2021-07-17 13:31:40 +0200

    Adjust supported Python versions.

--- setup.py
+++ setup.py
@@ -23,9 +23,10 @@ setup(
         'Environment :: Console',
         'Intended Audience :: System Administrators',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Topic :: System :: Installation/Setup',
         'Topic :: System :: Systems Administration'],
     include_package_data=True,
@@ -35,7 +36,7 @@ setup(
         'setuptools',
         'ploy >= 2.0.0b3',
         'lazy'],
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*',
     entry_points="""
         [ploy.plugins]
         virtualbox = ploy_virtualbox:plugin
